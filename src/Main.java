969acb43f6e9c685e9b3e2abf2fab8dbf6b4d931
xnzar <mustafacetin2043@gmail.com>
2023-12-23 21:08:20 +0300

    kitap ekleme metodu

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,16 @@ public class Main {
     static String[][] books = new String[INDEX][4];
     static String[][] patrons = new String[INDEX][4];
     static String[][] transactions = new String[INDEX][3];
+    static int kitapSayisi=0;
 
+    public static void addBook(String title,String author,String ISBN,String quantity){
+        books[kitapSayisi][0]=title;
+        books[kitapSayisi][1]=author;
+        books[kitapSayisi][2]=ISBN;
+        books[kitapSayisi][3]=quantity;
+        kitapSayisi++;
+
+    }
     public static void main(String[] args) {
 
     }
