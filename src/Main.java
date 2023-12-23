0ce0c40632be4f3ec71b2dcad9880dba8a5d7842
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-12-23 14:39:09 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        final int INDEX = 20;
+        String[][] books = new String[INDEX][4];
+        String[][] patrons = new String[INDEX][4];
+        String[][] transactions = new String[INDEX][3];
+    }
+}
