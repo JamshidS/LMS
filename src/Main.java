d716e3260f2a48ce0d3f9b0324bf58ea72cf4651
xnzar <mustafacetin2043@gmail.com>
2023-12-24 14:00:33 +0300

    quantity update

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,18 @@ public class Main {
     static String[][] books = new String[INDEX][4];
     static String[][] patrons = new String[INDEX][4];
     static String[][] transactions = new String[INDEX][3];
-    static int kitapSayisi=0;
+    static int bookQuantity=0;
 
-    public static void addBook(String title,String author,String ISBN,String quantity){
-        books[kitapSayisi][0]=title;
-        books[kitapSayisi][1]=author;
-        books[kitapSayisi][2]=ISBN;
-        books[kitapSayisi][3]=quantity;
-        kitapSayisi++;
-
+    public static void addBook(String title,String author,String ISBN){
+        if (bookQuantity<=INDEX){
+            books[bookQuantity][0]=title;
+            books[bookQuantity][1]=author;
+            books[bookQuantity][2]=ISBN;
+            bookQuantity++;
+        }
+        else {
+            System.out.println("The library capacity is full, new books cannot be added.");
+        }
     }
     public static void main(String[] args) {
 
