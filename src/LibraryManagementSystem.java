229798836c9bbf97472b8f35293ac5626a5c688a
xnzar <mustafacetin2043@gmail.com>
2024-01-01 12:37:18 +0300

    deleteBook add

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -47,5 +47,35 @@ public class LibraryManagementSystem {
             System.out.println();
         }
     }
+    public static void deleteBook(String ISBN){
+        int findIndex=-1;
+        for (int i=0; i<bookQuantity; i++) {
+            if (books[i][3].equals(ISBN)) {
+                findIndex = i;
+                break;
+            }
+        }
+        if (findIndex!=-1){
+            for (int i=findIndex; i<bookQuantity-1; i++){
+                books[i]=books[i+1];
+            }
+            bookQuantity--;
+            String[][] newBooks=new String[books.length][4];
+            for (int i=0; i<books.length; i++){
+                for (int j=0; j<4; j++){
+                    newBooks[i][j]=books[i][j];
+                }
+            }
+            books=newBooks;
+            System.out.println("Kitap Silinmiştir.");
+        }
+        if (bookQuantity==0){
+            System.out.println("Kütühanede kitap sayısı 0'dır.");
+        }
+        else {
+            System.out.println("Silmek isteğiniz kitap bulunmamaktadır.");
+
+        }
+    }
 
 }
