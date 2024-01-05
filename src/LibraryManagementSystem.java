b983cbb99e8220b9c3467a9e25497f41360fe2e6
emre aybar <aybaremre5@gmail.com>
2024-01-05 15:20:35 +0300

    n23

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -1,9 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class LibraryManagementSystem {
     static int INDEX = 20;
     static String[][] books = new String[INDEX][4];
     static String[][] patrons = new String[INDEX][4];
     static String[][] transactions = new String[INDEX][3];
     static int bookQuantity=0;
+
 
     int increase = 0;//transaciona bilgileri aktarma
     public void checkOutBook(String Tc, String bookISBN, String history) {
@@ -20,6 +25,52 @@ public class LibraryManagementSystem {
 
 
     }
+    private static void  checkBookReturnDeadline(String  bookISBN){
+
+
+        for (int i =0;i<transactions.length;i++){
+            String bookIsbn =transactions[i][1];
+            if (bookIsbn.equalsIgnoreCase(bookISBN)){
+                System.out.println(bookISBN+" book with ISBN number found.");
+
+                LocalDate today = LocalDate.now();
+                DateTimeFormatter format = DateTimeFormatter.ofPattern("d.M.yyyy");
+
+                System.out.println("Book purchased date           : " + format.format(today).toUpperCase());
+                LocalDate mustcome = today.plusDays(30);
+                System.out.println("Date the book should arrive : " + format.format(mustcome));
+
+
+                Scanner scan = new Scanner(System.in);
+                System.out.print("Date the book arrived (enter d.m.yyyy format): ");
+                String BookArrivalDate=scan.next();
+
+                try {
+                    LocalDate BookArrivalDatestr = LocalDate.parse(BookArrivalDate, format);
+                    System.out.println("Book purchased date        : " + format.format(BookArrivalDatestr));
+
+                    if (BookArrivalDatestr.isAfter(mustcome)) {
+                        System.out.println("The book arrived later than it was supposed to arrive!");
+                        System.out.println("The user is prohibited from purchasing books for 1 month.");
+                    } else {
+                        System.out.println("The book was delivered before its due date.");
+                        System.out.println("User can buy books.");
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("Incorrect date format! Please enter a date in d.M.yyyy format.");
+                }
+            }else {
+                System.out.println("No book record found.");
+
+            }
+        }
+
+
+
+
+    }
+
 
 
 
