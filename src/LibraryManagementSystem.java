0f713eac9577ed983c8c9b41d227d323ec9fb337
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-01-09 15:47:29 +0300

    Kıtap ıade işlemi

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -124,5 +124,40 @@ public class LibraryManagementSystem {
         }
 
     }
+    public static String bookReturn() {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("İsminizi giriniz :");
+        String name = scr.nextLine();
 
+        System.out.println("Kitabın ismini giriniz :");
+        String bookname = scr.nextLine();
+
+        System.out.println("Kitabın  numarasını  giriniz :");
+        int numarasi = scr.nextInt();
+
+        int kitapIndex = -1;
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] != null && books[i][0] != null && books[i][3] != null) {
+                if (books[i][0].equalsIgnoreCase(bookname) && books[i][3].equals(numarasi)) {
+                    kitapIndex = i;
+                    break;
+                }
+            }
+        }
+        if (kitapIndex != -1) {
+            int currentQuantity = Integer.parseInt(books[kitapIndex][2]);// Mevcut stok miktarını alır.
+            if (currentQuantity > 0) {
+                books[kitapIndex][2] = String.valueOf(currentQuantity - 1); // Stok miktarını bir azaltır ve dizide günceller.
+                System.out.println("İşlem başarıyla tamamlandı. Kitap iade edildi.");
+                return "İşlem başarıyla tamamlandı. Kitap iade edildi.";
+
+            } else {
+                System.out.println("kitap iade edilemedi. kitap stokta yok.");
+            }
+        } else {
+            System.out.println("Kitap iade edilemedi. Belirtilen isim ve numarada kitap bulunamadı veya stokta yok.");
+        }
+        scr.close();
+        return "Kitap iade edilemedi. Belirtilen isim ve numarada kitap bulunamadı veya stokta yok.";
+    }
 }
