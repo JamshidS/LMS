713e7bcafcb5204dfbc18817e93c93b88e72bbfc
melda <146105556+melda2300@users.noreply.github.com>
2024-01-10 12:49:21 +0300

    Update LibraryManagementSystem.java

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -126,22 +126,7 @@ public class LibraryManagementSystem {
     }
 
 
-    public static void updateBook(String ISBN, String title, String author, String bookPage) {
-        boolean kitapBulundu = false;
-        for (int i = 0; i < bookQuantity; i++) {
-            if (books[i][3].equals(ISBN)) {
-                kitapBulundu = true;
-                books[i][0] = title;
-                books[i][1] = author;
-                books[i][2] = bookPage;
-                System.out.println("Kitap Bilgileri Güncellenmiştir.");
-                break;
-            }
-        }
-        if (!kitapBulundu) {
-            System.out.println("Güncellemek istediğiniz kitap bulunmamaktadır.");
-        }
-    }
+ 
     public static String searchBooks() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Arama kriteri giriniz: ");
