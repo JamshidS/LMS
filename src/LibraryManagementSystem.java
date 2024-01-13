7139297a625e9e0ca99e86e7e7c72b1072e9f1ad
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-01-13 22:59:24 +0300

    kitap arama işlemi

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -125,34 +125,29 @@ public class LibraryManagementSystem {
 
     }
 
-    public static String searchBooks() {
+    public static void  searchBooks() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Arama kriteri giriniz: ");
-        String aramakriteri = scanner.next();
+        String  searchCriteria= scanner.next();
 
-        boolean tofind = false;
+        boolean toFind = false;
 
         for (int i = 0; i < bookQuantity; i++) {
-            if (books[i][0].equalsIgnoreCase(aramakriteri) || books[i][1].equalsIgnoreCase(aramakriteri) || books[i][2].equalsIgnoreCase(aramakriteri)) {
+            if (books[i][0].equalsIgnoreCase(searchCriteria) || books[i][1].equalsIgnoreCase(searchCriteria) || books[i][2].equalsIgnoreCase(searchCriteria)) {
                 System.out.println("Kitap Bulundu!");
                 System.out.println("Başlık: " + books[i][0]);
                 System.out.println("Yazar: " + books[i][1]);
                 System.out.println("ISBN: " + books[i][2]);
-                tofind = true;
+                toFind = true;
                 break;
             }
         }
 
-        if (!tofind) {
+        if (!toFind) {
             System.out.println("Kitap bulunamadı.");
 
         }
-
-        return searchBooks();
-
     }
- 
-
 }
 
 
