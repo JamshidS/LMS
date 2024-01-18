bc88eafd23f1b97094d6076db33d3f0a40700f1e
melda2300 <146105556+melda2300@users.noreply.github.com>
2024-01-19 00:08:33 +0300

    git add .

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -294,27 +294,7 @@ public class LibraryManagementSystem {
             }
         }
     }
-        public static void  searchBooks() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Arama kriteri giriniz: ");
-            String searchCriteria = scanner.next();
-
-            boolean toFind = false;
-
-            for (int i = 0; i < bookQuantity; i++) {
-                if (books[i][0].equalsIgnoreCase(searchCriteria) || books[i][1].equalsIgnoreCase(searchCriteria) || books[i][2].equalsIgnoreCase(searchCriteria)) {
-                    System.out.println("Kitap Bulundu!");
-                    System.out.println("Başlık: " + books[i][0]);
-                    System.out.println("Yazar: " + books[i][1]);
-                    System.out.println("ISBN: " + books[i][2]);
-                    toFind = true;
-                    break;
-                }
-            }
-            if (!toFind) {
-                System.out.println("Kitap bulunamadı.");
-            }
-        }
+      
     public static void bookview(String bookName) {
 
         boolean kitapBulundu = false;
