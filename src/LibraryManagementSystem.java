d25fdd6d668067ca07f29945ecbca8362f225c34
Tunahan Basturk <tunahan.basturk1907@gmail.com>
2024-01-21 21:43:55 +0300

    tunahan/NO-05-book-return-process

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -388,32 +388,14 @@ public class LibraryManagementSystem {
         return bookQuantity;
 
     }
-    public static String bookReturn(String name, String bookName, String bookISBN) {
-        int kitapIndex = -1;
-        for (int i = 0; i < books.length; i++) {
-            if (books[i] != null && books[i][0] != null && books[i][3] != null) {
-                if (books[i][0].equalsIgnoreCase(bookName) && books[i][3].equals(bookISBN)) {
-                    kitapIndex = i;
-                    break;
-                }
-            }
-        }
-        if (kitapIndex != -1) {
-            int currentQuantity = Integer.parseInt(books[kitapIndex][2]);
-            if (currentQuantity > 0) {
-                books[kitapIndex][2] = String.valueOf(currentQuantity - 1);
-                System.out.println("İşlem başarıyla tamamlandı. Kitap iade edildi.");
-
+    public static void bookReturn(String tcNo, String bookISBN) {
                 int transactionIndex = -1;
                 for (int i = 0; i < transactions.length; i++) {
-                    if (transactions[i] != null && transactions[i][0] != null && transactions[i][1] != null && transactions[i][2] != null) {
-                        if (transactions[i][0].equalsIgnoreCase(bookName) && transactions[i][1].equals(bookISBN)) {
+                        if (transactions[i][0].equalsIgnoreCase(tcNo) && transactions[i][1].equals(bookISBN)) {
                             transactionIndex = i;
                             break;
                         }
                     }
-                }
-
                 if (transactionIndex != -1) {
                     for (int i = transactionIndex; i < transactions.length - 1; i++) {
                         transactions[i] = transactions[i + 1];
@@ -421,19 +403,7 @@ public class LibraryManagementSystem {
                     transactions[transactions.length - 1] = null;
                     System.out.println("Kitap iade edildi ve ilgili işlem kaydı silindi.");
                 } else {
-                    System.out.println("Kitap iade edildi, ancak ilgili işlem kaydı bulunamadı.");
+                    System.out.println("Kitap iade edilemedi, ilgili işlem kaydı bulunamadı.");
                 }
-                System.out.println("İşlem başarıyla tamamlandı. Kitap iade edildi.");
-            } else {
-                System.out.println("kitap iade edilemedi. kitap stokta yok.");
-            }
-        } else {
-            System.out.println("Kitap iade edilemedi. Belirtilen isim ve numarada kitap bulunamadı veya stokta yok.");
-        }
-        return "Kitap iade edilemedi. Belirtilen isim ve numarada kitap bulunamadı veya stokta yok.";
     }
-    }
-
 }
-
-
